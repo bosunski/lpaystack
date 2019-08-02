512b1e3b796e8c741824d9103203ec5867fd3114
wisdomanthoni <wizitendo10@gmail.com>
2019-08-02 02:10:18 +0100

    u

--- src/Paystack.php
+++ src/Paystack.php
@@ -120,6 +120,7 @@ class Paystack
      * Get the authorization url from the callback response.
      *
      * @param null $data
+     * 
      * @return Paystack
      */
     public function getAuthorizationUrl($data = null)
